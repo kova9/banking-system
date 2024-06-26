3d8d8c73a3f5e27b85b0e6ee51e05dfc8cb907a4
KovacevicTom <kovacevictom@sbsit.com>
2024-06-26 09:15:58 +0200

    add swagger ui

--- src/main/java/com/bankingSystem/bankingSystem/controller/TransactionController.java
+++ src/main/java/com/bankingSystem/bankingSystem/controller/TransactionController.java
@@ -1,9 +1,13 @@
 package com.bankingSystem.bankingSystem.controller;
 
 import com.bankingSystem.bankingSystem.dataaccess.entity.Transaction;
+import com.bankingSystem.bankingSystem.dto.TransactionDto;
 import com.bankingSystem.bankingSystem.obj.TransactionResponse;
 import com.bankingSystem.bankingSystem.service.TransactionService;
 import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +25,13 @@ public class TransactionController {
     }
 
     @PostMapping("/")
+    @Parameter(name = "senderAccountId", example = "cf3d5e48-1e8b-4c5c-877c-8d1b4239f489", required = true)
+    @Parameter(name = "receiverAccountId", example = "ac0c4d5b-725e-4e0c-9a57-9d6184b7d5c7", required = true)
+    @Parameter(name = "amount", example = "1000", required = true)
+    @Parameter(name = "currencyId", example = "EUR", required = true)
+    @Parameter(name = "message", example = "TestMessage", required = false)
+    @Parameter(name = "storno", example = "false", required = false)
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Transaction to add", required = true, content = @Content(schema = @Schema(implementation = TransactionDto.class)))
     public ResponseEntity<TransactionResponse> saveTransaction(@RequestBody JsonNode in){
         return transactionService.saveTransaction(in);
     }
